8f9ec57ddac0361cf7a8e333edf25d5c9fbb6e22
Andrei Shustov <shustov_a@mail.ru>
2020-08-23 19:20:32 +1000

    First commit for test

---
+++ ChangeViewColor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChangeViewColor
+//
+//  Created by Андрей Шустов on 23.08.2020.
+//  Copyright © 2020 Andrei Shustov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
